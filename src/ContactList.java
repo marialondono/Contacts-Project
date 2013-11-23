a9d974573a85062df0e3272bf761e9ee6a7f815d
Maria Londono <tennisplz305@gmail.com>
2013-11-22 17:15:15 -0800

    directory variable that keeps the directory

--- src/ContactList.java
+++ src/ContactList.java
@@ -1,8 +1,10 @@
-
 public class ContactList {
+	private static Directory directory;
 
 	public static void main(String[] args) {
 		System.out.println("hello");
 	}
 
+	public static void loadContactsFromDisk() {
+	}
 }
