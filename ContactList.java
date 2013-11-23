4d734c912c27cd02f161bd567f0d9a1b7da217a3
Maria Londono <tennisplz305@gmail.com>
2013-11-22 19:43:09 -0800

    new classes with the info

---
+++ ContactList.java
@@ -0,0 +1,30 @@
+public class ContactList {
+	private static Directory directory;
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+	}
+
+	public static void loadContactsFromDisk() {
+		
+	}
+	
+	public static void printMainMenu() {
+		
+	}
+	
+	public static void addContact(){
+		
+	}
+	
+	public static void printListofContacts() {
+		
+	}
+	
+	public static void searchByLastName() {
+		
+	}
+	public static void saveAndQuit() {
+		
+	}
+}
