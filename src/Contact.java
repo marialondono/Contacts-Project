70b161ec1710e02becccd6452ea567566b782a44
licecolony <bismuthalex@gmail.com>
2013-11-22 17:13:43 -0800

    Settling Variables as Well as Methods
    Added private strings to the coding
    firstname,lastname,streetaddress,email,phone,and notes as well as
    defining methods to get and set the information

--- src/Contact.java
+++ src/Contact.java
@@ -1,4 +1,38 @@
-
 public class Contact {
+	private String firstName;
+	private String lastName;
+	private String streetAddress;
+	private String emailAddress;
+	private String phoneNumber;
+	private String notes;
 
+	/**
+	 * Method getFirstName will ask user to input first name
+	 */
+	public void getFirstName() {
+
+	}
+
+	/**
+	 * Method setFirstName will store user's first name as variable
+	 * newFirstName;
+	 * 
+	 */
+	public void setFirstName() {
+
+	}
+
+	/**
+	 * Method getLastName will ask user to input Last name
+	 */
+	public void getLastName() {
+
+	}
+
+	/**
+	 * Method setLastName will store user's Last name as variable newFirstName;
+	 * 
+	 */
+	public void setLastName() {
+	}
 }
